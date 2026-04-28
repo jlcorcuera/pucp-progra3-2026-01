63cec5c490610fd245d7318fef6331d35522c4a2
José Luis Corcuera Bárcena <jlcorcuera@gmail.com>
2026-04-28 18:21:54 -0500

    Fixed conflicts

--- unit-04-database-persistence-and-connectivity/Lab06SoftProg/Lab06SoftProg-dao/src/main/java/pe/edu/pucp/lab06/softprog/dao/BaseDAO.java
+++ unit-04-database-persistence-and-connectivity/Lab06SoftProg/Lab06SoftProg-dao/src/main/java/pe/edu/pucp/lab06/softprog/dao/BaseDAO.java
@@ -1,20 +1,9 @@
 package pe.edu.pucp.lab06.softprog.dao;
 
-<<<<<<< HEAD
-import java.sql.SQLException;
-
 public interface BaseDAO <E, ID> {
 
     E save(E e) throws Exception;
     void remove(E e) throws Exception;
     E load(ID id) throws Exception;
     E update(E e) throws Exception;
-=======
-public interface BaseDAO <E, ID> {
-
-    E save(E e);
-    void remove(E e);
-    E load(ID id);
-    E update(E e);
->>>>>>> 4015b73e8b8686efecc5246e9bc696a45b4c1a97
 }
